98113a222fe3331ebb0bfde728bdb30ad4698448
ReFantasy <309741309@qq.com>
2019-10-05 21:03:57 +0800

    update

---
+++ ThreadPool/sync_queue.h
@@ -0,0 +1,4 @@
+#ifndef __SYNC_QUEUE_H__
+#define __SYNC_QUEUE_H__
+
+#endif//__SYNC_QUEUE_H__
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ThreadPool/sync_queue.h"
+using namespace std;
+
+int main()
+{
+	cout << "Hello World~!" << endl;
+
+	
+	
+	return 0;
+}
